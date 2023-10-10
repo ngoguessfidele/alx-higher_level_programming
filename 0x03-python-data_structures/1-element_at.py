60e76d064cf31947dddb64289792dc962c01395e
ngoguessfidele <ngoguessfidele@gmail.com>
2023-10-11 00:13:58 +0200

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0:
         return (None)
-    return mylist[idx]
+    len1 = len(my_list)
+
+    if idx > len1 - 1:
+        return (None)
+
+    return (my_list[idx])
