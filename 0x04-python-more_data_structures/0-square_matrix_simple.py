d7eb654253bb479d3ce9c22ab5c7942062693621
ngoguessfidele <ngoguessfidele@gmail.com>
2023-10-12 04:57:00 +0200

    Task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    thematrix = matrix.copy()
+    for i in range(len(matrix)):
+        thematrix[i] = list(map(lambda x: x**2, matrix[i]))
+    return the matrix
