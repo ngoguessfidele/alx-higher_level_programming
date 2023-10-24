15f91a86efca394dc0568b546c2be7d76d89d2a8
ngoguessfidele <ngoguessfidele@gmail.com>
2023-10-24 04:06:59 +0200

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            n += 1
+
+    print()
+    
+    return (n)
