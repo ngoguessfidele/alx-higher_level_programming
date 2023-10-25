48fa657be337e8a133bd2fdf75a46ab74ebfd4c4
ngoguessfidele <ngoguessfidele@gmail.com>
2023-10-25 05:07:09 +0200

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,8 @@ def no_c(my_string):
 
     i = 0
 
+    new = my_string[:]
+
     for j in range(n):
         if (my_string[j] == 'c' or my_string[j] == 'C'):
             new = new[:(j - i)] + my_string[(j + 1):]
