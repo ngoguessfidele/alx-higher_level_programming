a8efbf23dd45e254ad04d8d8e4e3285f9084372a
ngoguessfidele <ngoguessfidele@gmail.com>
2023-10-25 03:45:09 +0200

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+    n = len(sys.argv) - 1
+
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+
+    if  n > 1:
+        n = 0
+        for arg in sys.argv:
+            if n != 0:
+                print("{}: {}".format(n, arg))
+            n += 1
