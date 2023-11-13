376aeb4c337e28d04d5163798bad1d99a39f7017
ngoguessfidele <ngoguessfidele@gmail.com>
2023-11-13 19:47:48 +0200

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@ def add_integer(a, b=98):
     """Adds a and b"""
 
     try:
-        if not(isinstance(a, int)) and not isinstance(a, float):
+        if not (isinstance(a, int)) and not isinstance(a, float):
             raise TypeError("a must be an integer")
         elif not (isinstance(b, int)) and not isinstance(b, float):
             raise TypeError("b must be an integer")
